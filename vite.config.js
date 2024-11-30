fe8f372670eac366d9005711261b0b3101756d4d
Savi Ugale <106169833+savi-ugale@users.noreply.github.com>
2024-11-30 16:52:11 +0530

    Updated vite.config.js to add alias for cleaner imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path'; // Add resolve from path module
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      '@': resolve(__dirname, 'src'), // Alias for the src folder
+    },
+  },
+});
